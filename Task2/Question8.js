4584067abf6adb2c12334faf4cb1e4f59981ab1e
kaushalkumar98 <kaushalkumar3795@gmail.com>
2023-07-24 00:26:40 +0530

    Question8Task2

---
+++ Task2/Question8.js
@@ -0,0 +1,59 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+   // Create edit button element
+   var editBtn = document.createElement('button');
+
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  // Add classes to edit button
+  editBtn.className = 'btn btn-warning btn-sm float-right edit';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+    // Append text node
+    editBtn.appendChild(document.createTextNode('Edit'));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+  // Append button to li
+  li.appendChild(editBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+  // Append li to list
+  itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
