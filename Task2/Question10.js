4ec74da84af684443a008a31919fcff3c10c16a7
kaushalkumar98 <kaushalkumar3795@gmail.com>
2023-07-24 10:23:39 +0530

    Question10Task2

--- Task2/Question10.js
+++ Task2/Question10.js
@@ -14,31 +14,7 @@
         // Get the values entered by the user
         const name = nameInput.value.trim();
         const email = emailInput.value.trim();
+        localStorage.setItem('userDetails', userJSON);
 
-        // Check if the name and email are not empty
-        if (name !== '' && email !== '') {
-            // Create a user object with the input data
-            const user = {
-                name: name,
-                email: email
-            };
-
-            // Convert the user object to a JSON string
-            const userJSON = JSON.stringify(user);
-
-            // Store the JSON string in the local storage
-            localStorage.setItem('userDetails', userJSON);
-
-            // Clear the form fields
-            nameInput.value = '';
-            emailInput.value = '';
-
-            // Display a success message
-            const msgDiv = document.querySelector('.msg');
-            msgDiv.innerHTML = 'User details have been successfully stored.';
-        } else {
-            // Display an error message if any field is empty
-            const msgDiv = document.querySelector('.msg');
-            msgDiv.innerHTML = 'Please enter both name and email.';
-        }
+        
     });
